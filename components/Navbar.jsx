675ada4d862db9cfb4dc463e7e90c8d9df590db1
riteekagawand <riteekagawand7@gmail.com>
2024-08-01 19:02:24 +0530

    Ensured UI updates consistently reflect the latest server data

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
        <p className="text-[30px] text-white">To Do List</p>
       </Link>
       <Link className="bg-gray-200 p-2 mr-6 rounded-2xl  hover:bg-gray-400 border hover:border-white" href={"/addTodo"}>
-        <p className=" text-[20px] text-slate-800 hover:text-white hover:border-white">Add To Do's</p>
+      <p className="text-[20px] text-slate-800 hover:text-white hover:border-white">Add To Do&apos;s</p>
       </Link>
     </nav>
   );
